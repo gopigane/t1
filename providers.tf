7e3c6766d5d2b1401d264416db5d2edb528f3d89
gopigane <92424505+gopigane@users.noreply.github.com>
2022-07-05 11:20:43 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,4 +1,4 @@
 provider "aws" {
-  shared_config_files      = ["/root/.aws/config"]
-  shared_credentials_files = ["/root/.aws/credentials"]
+  shared_config_files      = ["/home/ubuntu/.aws/config"]
+  shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
 }
